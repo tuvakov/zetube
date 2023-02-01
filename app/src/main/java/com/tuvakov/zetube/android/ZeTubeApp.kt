5aa3199c38ba7707b72008246624780ad91c0abe
Jemshit Tuvakov <jemshit.tuvakov@gmail.com>
2023-02-01 15:43:37 +0900

    Update kotlin version. Setup timber.

--- app/src/main/java/com/tuvakov/zetube/android/ZeTubeApp.kt
+++ app/src/main/java/com/tuvakov/zetube/android/ZeTubeApp.kt
@@ -2,7 +2,27 @@ package com.tuvakov.zetube.android
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class ZeTubeApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        setupTimber()
+    }
+
+    private fun setupTimber() {
+        val tree = object : Timber.DebugTree() {
+            override fun log(
+                priority: Int,
+                tag: String?,
+                message: String,
+                t: Throwable?
+            ) {
+                super.log(priority, "${this@ZeTubeApp::class.simpleName}: $tag", message, t)
+            }
+        }
+        Timber.plant(tree)
+    }
 }
